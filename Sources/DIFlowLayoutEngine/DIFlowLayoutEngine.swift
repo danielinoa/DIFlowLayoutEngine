115275934ad89c7b9f815fa8db4ae2688ac1259c
Daniel Inoa <danielinoa@gmail.com>
2023-01-04 23:37:54 -0800

    Add initial implementation

--- Sources/DIFlowLayoutEngine/DIFlowLayoutEngine.swift
+++ Sources/DIFlowLayoutEngine/DIFlowLayoutEngine.swift
@@ -1,6 +1,169 @@
+/// This engine computes the positions of items within a containing bound adopting a flow layout,
+/// where items are arranged horizontally and wrapped vertically.
 public struct DIFlowLayoutEngine {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+    /// The direction items flow within a row.
+    public var direction: Direction = .forward
+
+    /// The horizontal alignment of items within a row.
+    public var horizontalAlignment: HorizontalAlignment = .leading
+
+    /// The vertical alignment of items within a row.
+    public var verticalAlignment: VerticalAlignment = .top
+
+    /// The horizontal distance between adjacent items within a row.
+    public var horizontalSpacing: Double = .zero
+
+    /// The vertical distance between adjacent rows.
+    public var verticalSpacing: Double = .zero
+
+    // MARK: - Layout Calculation
+
+    /// Returns the positions of the items within the specified bounds,
+    /// and the height require to fit all items within the bounds.
+    public func position(of items: [Rectangle], in bounds: Rectangle) -> Layout {
+        let (rows, totalHeight) = rows(
+            from: items, in: bounds, horizontalSpacing: horizontalSpacing, verticalSpacing: verticalSpacing
+        )
+        var positions: [Position] = []
+        for row in rows {
+            var leadingOffset = initialLeadingOffset(
+                for: row, in: bounds, alignment: horizontalAlignment, horizontalSpacing: horizontalSpacing
+            )
+            var rowPositions: [Position] = []
+            for rectangle in row.items {
+                let topOffset = topOffset(for: rectangle, aligned: verticalAlignment, within: row)
+                rowPositions.append(.init(x: leadingOffset, y: topOffset))
+                leadingOffset += rectangle.width + horizontalSpacing
+            }
+            if direction == .reverse { rowPositions.reverse() }
+            positions.append(contentsOf: rowPositions)
+        }
+        return Layout(
+            fittingHeight: totalHeight,
+            positions: positions
+        )
+    }
+
+    // MARK: - Row Grouping
+
+    /// This function groups items into rows based on the available width defined by the bounds
+    /// and the specified spacing.
+    private func rows(
+        from items: [Rectangle], in bounds: Rectangle, horizontalSpacing: Double, verticalSpacing: Double
+    ) -> (rows: [Row], totalHeight: Double) {
+        var items = items
+        var rows: [Row] = []
+        while !items.isEmpty {
+            let topOffset = rows.last.map { $0.topOffset + $0.height + verticalSpacing  } ?? bounds.minY
+            var row = Row(topOffset: topOffset)
+            var isOverflown = false
+            while (!isOverflown && !items.isEmpty) {
+                let item = items.removeFirst()
+                row.items.append(item)
+                row.totalItemsWidth += item.width
+                row.height = max(row.height, item.height)
+                let currentLeadingOffset = row.items.last.map { $0.width + horizontalSpacing } ?? bounds.minX
+                let nextItem = items.first
+                isOverflown = nextItem.map { (currentLeadingOffset + $0.width) > bounds.maxX } ?? false
+            }
+            rows.append(row)
+        }
+        let verticalGapsCount = rows.count > 1 ? rows.count - 1 : .zero
+        let totalHeight = rows.map(\.height).reduce(.zero, +) + (Double(verticalGapsCount) * verticalSpacing)
+        return (rows, totalHeight)
+    }
+
+    // MARK: - In-Row Vertical Positioning
+
+    private func topOffset(for item: Rectangle, aligned: VerticalAlignment, within row: Row) -> Double {
+        let shift: Double
+        switch aligned {
+        case .top: shift = .zero
+        case .center: shift = (row.height - item.height) / 2
+        case .bottom: shift = row.height - item.height
+        }
+        return row.topOffset + shift
+    }
+
+    // MARK: - In-Row Horizontal Positioning
+
+    /// Returns the leading offset the row's first item can be placed in.
+    private func initialLeadingOffset(
+        for row: Row, in bounds: Rectangle, alignment: HorizontalAlignment, horizontalSpacing: Double
+    ) -> Double {
+        let gaps: Int = row.items.count == 1 ? .zero : row.items.count - 1
+        let gapsWidth = Double(gaps) * horizontalSpacing
+        let remainingSpace = bounds.width - (row.totalItemsWidth + gapsWidth)
+        let shift: Double
+        switch alignment {
+        case .leading: shift = .zero
+        case .center: shift = remainingSpace / 2
+        case .trailing: shift = remainingSpace
+        }
+        return bounds.minX + shift
+    }
+
+    // MARK: - Public Types
+
+    public struct Layout {
+
+        /// The height require to fit all items, based on the width of the bounds originally passed in.
+        let fittingHeight: Double
+
+        /// The position of the items within fitting height and the bounds' width.
+        let positions: [Position]
+    }
+
+    /// The direction items flow within a row.
+    public enum Direction {
+
+        /// In this direction items flow from left to right.
+        case forward
+
+        /// In this direction items flow from right to left.
+        case reverse
+    }
+
+    /// The horizontal alignment of items within a row.
+    public enum HorizontalAlignment {
+        case leading, center, trailing
+    }
+
+
+    /// The vertical alignment of items within a row.
+    public enum VerticalAlignment {
+        case top, center, bottom // TODO: Add baseline vertical alignment.
+    }
+
+    public struct Position: Equatable {
+        var x: Double, y: Double
+    }
+
+    public struct Rectangle: Equatable {
+        var x: Double, y: Double, width: Double, height: Double
+        var minY: Double { y }
+        var midY: Double { (height / 2) + y }
+        var maxY: Double { height + y }
+        var minX: Double { x }
+        var midX: Double { (width / 2) + x }
+        var maxX: Double { width + x }
+        var center: Position { .init(x: midX, y: midY) }
+    }
+
+    // MARK: - Auxiliary Types
+
+    private struct Row {
+
+        var items: [Rectangle] = []
+
+        /// The offset from the container-bounds' min-y (not necessarily zero).
+        var topOffset: Double = .zero
+
+        /// The height of the row, based on the tallest item within the row.
+        var height: Double = .zero
+
+        /// The sum of all the items' widths. This does not include any interim spacing.
+        var totalItemsWidth: Double = .zero
     }
 }
--- Tests/DIFlowLayoutEngineTests/DIFlowLayoutEngineTests.swift
+++ Tests/DIFlowLayoutEngineTests/DIFlowLayoutEngineTests.swift
@@ -2,10 +2,109 @@ import XCTest
 @testable import DIFlowLayoutEngine
 
 final class DIFlowLayoutEngineTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DIFlowLayoutEngine().text, "Hello, World!")
+
+    typealias Rectangle = DIFlowLayoutEngine.Rectangle
+
+    func testZeroItems() throws {
+        let engine = DIFlowLayoutEngine()
+        let bounds = Rectangle(width: 100, height: 100)
+        let layout = engine.position(of: [], in: bounds)
+        XCTAssertEqual(layout.positions.count, .zero)
+        XCTAssertEqual(layout.positions.first, nil)
+        XCTAssertEqual(layout.fittingHeight, .zero)
     }
+
+    func testOneItem() {
+        let engine = DIFlowLayoutEngine()
+        let item = Rectangle(width: 20, height: 20)
+        let bounds = Rectangle(width: 100, height: 100)
+        let layout = engine.position(of: [item], in: bounds)
+
+        XCTAssertEqual(layout.positions.count, 1)
+        XCTAssertEqual(layout.positions.first, .zero)
+        XCTAssertEqual(layout.fittingHeight, 20)
+    }
+
+    func testOneItemWithVerticalSpacing() throws {
+        let engine = DIFlowLayoutEngine(verticalSpacing: 100)
+        let item = Rectangle(width: 20, height: 20)
+        let bounds = Rectangle(width: 100, height: 100)
+        let layout = engine.position(of: [item], in: bounds)
+
+        XCTAssertEqual(layout.positions.count, 1)
+        XCTAssertEqual(layout.positions.first, .zero)
+        XCTAssertEqual(layout.fittingHeight, 20)
+    }
+
+    func testOneItemCentered() {
+        let engine = DIFlowLayoutEngine(horizontalAlignment: .center)
+        let item = Rectangle(width: 20, height: 20)
+        let bounds = Rectangle(width: 100, height: 100)
+        let layout = engine.position(of: [item], in: bounds)
+
+        XCTAssertEqual(layout.positions.count, 1)
+        XCTAssertEqual(layout.positions.first, .init(x: 40, y: .zero))
+        XCTAssertEqual(layout.fittingHeight, 20)
+    }
+
+    func testTwoItemsWithHorizontalSpacing() {
+        let engine = DIFlowLayoutEngine(horizontalSpacing: 10)
+        let item1 = Rectangle(width: 20, height: 20)
+        let item2 = Rectangle(width: 40, height: 40)
+        let bounds = Rectangle(width: 100, height: 100)
+        let layout = engine.position(of: [item1, item2], in: bounds)
+
+        XCTAssertEqual(layout.positions.count, 2)
+        XCTAssertEqual(layout.positions.first, .zero)
+        XCTAssertEqual(layout.positions.last, .init(x: 30, y: .zero))
+        XCTAssertEqual(layout.fittingHeight, 40)
+    }
+
+    func testThreeItemsWithHorizontalSpacingAndOverflowing() {
+        let engine = DIFlowLayoutEngine(horizontalSpacing: 20)
+        let item1 = Rectangle(width: 20, height: 20)
+        let item2 = Rectangle(width: 70, height: 30)
+        let item3 = Rectangle(width: 10, height: 10)
+        let bounds = Rectangle(width: 100, height: 100)
+        let layout = engine.position(of: [item1, item2, item3], in: bounds)
+
+        let item1Position = layout.positions[0]
+        let item2Position = layout.positions[1]
+        let item3Position = layout.positions[2]
+
+        XCTAssertEqual(layout.positions.count, 3)
+        XCTAssertEqual(item1Position, .zero)
+        XCTAssertEqual(item2Position, .init(x: .zero, y: 20))
+        XCTAssertEqual(item3Position, .init(x: 90, y: 20))
+        XCTAssertEqual(layout.fittingHeight, 50)
+    }
+
+    func testThreeItemsWithHorizontalSpacingAndVerticalSpacingAndOverflowingAndCentered() {
+        let engine = DIFlowLayoutEngine(horizontalAlignment: .center, horizontalSpacing: 20, verticalSpacing: 20)
+        let item1 = Rectangle(width: 20, height: 20)
+        let item2 = Rectangle(width: 70, height: 30)
+        let item3 = Rectangle(width: 10, height: 10)
+        let bounds = Rectangle(width: 100, height: 100)
+        let layout = engine.position(of: [item1, item2, item3], in: bounds)
+
+        let item1Position = layout.positions[0]
+        let item2Position = layout.positions[1]
+        let item3Position = layout.positions[2]
+
+        XCTAssertEqual(layout.positions.count, 3)
+        XCTAssertEqual(item1Position, .init(x: 40, y: .zero))
+        XCTAssertEqual(item2Position, .init(x: 0, y: 40))
+        XCTAssertEqual(item3Position, .init(x: 90, y: 40))
+        XCTAssertEqual(layout.fittingHeight, 70)
+    }
+}
+
+extension DIFlowLayoutEngine.Rectangle {
+    init(width: Double, height: Double) {
+        self = .init(x: .zero, y: .zero, width: width, height: height)
+    }
+}
+
+extension DIFlowLayoutEngine.Position {
+    static var zero: Self { .init(x: .zero, y: .zero) }
 }
